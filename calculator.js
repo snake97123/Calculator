cd6a31d250bfb1351e2af27554c92bc55d3c7541
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-09-05 13:36:21 +0900

    四則演算の実装

--- calculator.js
+++ calculator.js
@@ -4,10 +4,58 @@ const operatorButtons = document.querySelectorAll('#operator');
 const resetButton = document.querySelector('#reset');
 const equalButton = document.querySelector('#equal');
 
+let storedValue = null;
+let currentOperator = null;
+
 numButtons.forEach(button => {
   button.addEventListener('click', () => {
     result.value += button.getAttribute('data-value')
   });
+});
+
+document.querySelector('#operator[data-value="+"]').addEventListener('click', () => {
+      storedValue = parseFloat(result.value);
+      currentOperator = '+';
+      result.value = '';
+});
+
+document.querySelector('#operator[data-value="-"]').addEventListener('click', () => {
+  storedValue = parseFloat(result.value);
+  currentOperator = '-';
+  result.value = '';
+});
+
+document.querySelector('#operator[data-value="×"]').addEventListener('click', () => {
+  storedValue = parseFloat(result.value);
+  currentOperator = '×';
+  result.value = '';
+});
+
+document.querySelector('#operator[data-value="÷"]').addEventListener('click', () => {
+  storedValue = parseFloat(result.value);
+  currentOperator = '÷';
+  result.value = '';
+});
+
+document.querySelector('#calculate').addEventListener('click', () => {
+    if(storedValue !== null && currentOperator !== null) {
+      switch(currentOperator) {
+        case '+':
+          result.value = storedValue + parseFloat(result.value);
+          break;
+        case '-':
+          result.value = storedValue - parseFloat(result.value);
+          break;
+        case '×':
+          result.value = storedValue * parseFloat(result.value);
+          break;
+        case '÷':
+          result.value = storedValue / parseFloat(result.value);
+          break;
+      }
+      storedValue = null;
+      currentOperator = null;
+    }
 });
 
 resetButton.addEventListener('click', () => {
